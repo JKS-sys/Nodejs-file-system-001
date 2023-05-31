f5128b98402fedc0ce702603b6b0ec197a08f0a7
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-05-31 13:36:32 +0530

    Commit

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const { response } = require("express");
 const fs = require("fs");
+
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
+
+
 var revil = [];
 
-getDateString=()=> {
+getDateString = () => {
+
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     const date = new Date();
+    // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
+    let d = weekday[date.getDay()];
     var currentTimeInSeconds = Math.floor(Date.now() / 1000);
     const year = date.getFullYear();
+    // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
     const month = `${date.getMonth() + 1}`.padStart(2, '0');
     const day = `${date.getDate()}`.padStart(2, '0');
     var hour = date.getHours();
+    // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
-    return `${year},${month} ${day}, ${hour}-${minutes}-${seconds}` 
+    return `${year},${month} ${day}, ${hour}-${minutes}-${seconds}, ${d}`
 }
-
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
 
 var data = getDateString();
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
+
 const date = new Date();
 let time = date.getTime();
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
 
-const express = require("express");   
+const express = require("express");
 const app = express();
 const PORT = 3000;
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
 
-fs.writeFile(`./files/${data}.txt`, `Date--${data} && Timestamp--${time}`,
- (err) => console.log("hi there project success"));
+fs.writeFile(`./files/${data}`, `Date--${data} && Timestamp--${time}`,
+    (err) => console.log("hi there project success"));
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
 
-fs.readFile(`./files/${data}.txt`, "utf-8", (err, data) => { 
+fs.readFile(`./files/${data}`, "utf-8", (err, data) => {
     console.log(data);
     app.get("/", (request, response) => {
         response.send(data);
@@ -34,17 +50,21 @@ fs.readFile(`./files/${data}.txt`, "utf-8", (err, data) => {
     app.listen(PORT, () => console.log("App is started", PORT));
 }
 );
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
 
 const Store = './files';
 
 fs.readdir(Store, (err, files) => {
-   
+    // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
+
     files.forEach(file => {
         console.log(file);
         revil.push(file);
     });
 
 });
+// Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
+
 app.get("/files", (request, response) => {
     response.send(revil);
 });
