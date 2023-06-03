330d268028fa6d9464b3ad531c2de5c5e11af3d2
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-06-03 16:17:07 +0530

    Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ getDateString = () => {
     const date = new Date();
     // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
     let d = weekday[date.getDay()];
-    let m = monthNamelist[1+(date.getDay())];
+    let m = monthNamelist[(date.getDay())-1];
     var currentTimeInSeconds = Math.floor(Date.now() / 1000);
     const year = date.getFullYear();
     // Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@airtel .
